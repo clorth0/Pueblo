991c7566b1ece22f30e8698a334e4895064286a1
Matt James <6087477+clorth0@users.noreply.github.com>
2023-10-04 11:26:47 -0500

    Update build_site.py

--- build_site.py
+++ build_site.py
@@ -3,8 +3,8 @@
 import pueblo
 
 PARAMS = {
-	'DIR': '/dir/to/your/html/files/',
-	'TEMPLATE_DIR': '/dir/to/store/your/templates/',
+	'DIR': '/Users/matt/Documents/GitHub/www-odnd',
+	'TEMPLATE_DIR': '/Users/matt/Documents/GitHub/www-odnd/Pueblo/templates',
 	'IGNORE_LIST': ['ignorethis.txt'],
 	'PAGEBUILD_DELTA': 30
 	}
@@ -14,5 +14,5 @@ print "Content-type: text/html\n\n"
 site = pueblo.Site()
 site.build_site(PARAMS)
 
-print "<html><head><title>Site Rebuilt</title></head><body><h1>Site Rebuilt</h1></body></html>"
+print "<html><head><title>Site Rebuilt</title></head><body><h1>Security Consulting</h1></body></html>"
 
